031ec34f6c74446c4550cde2d7de6a321ea3a876
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-18 06:56:09 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,6 +2,9 @@ alert("hello world");
 const kitties = ["барсик","фунтик","кузя"]
 
 for (let key in kitties) {
-    if (key.length == 6)
+    if (key.length == 6){
     return("Вот такие пироги")
+    }else {
+        alert("Вот такие пирожные")
+    }
 }
