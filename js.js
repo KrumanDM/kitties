4d0b05618bb04d5a40a6c5cc8dbbfc085daec52b
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-19 07:20:00 +0300

    Update js.js

--- js.js
+++ js.js
@@ -6,5 +6,17 @@ for (let key in kitties) {
     return("Вот такие пироги")
     }else {
         alert("Вот такие пирожные")
-    }
+    } 
 }
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if sr
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
