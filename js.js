b58bf01cbd1cad6d0485c8a5decd9ccbed5b87f0
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-21 12:03:50 +0300

    Update js.js

--- js.js
+++ js.js
@@ -94,3 +94,23 @@ return undefined
 }
 
 abTest(2,2);
+
+
+
+
+
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if (card <= 6) 
+    count++;
+  else if (card >="10")
+    count--;
+  return count + (count > 0 ? ' Bet' : ' Hold');
+}
+  // Only change code above this line
+
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
