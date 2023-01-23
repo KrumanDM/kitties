75e6fa747148b292cd4b05fb10456e1ef8e37010
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-23 20:44:57 +0300

    Update js.js

--- js.js
+++ js.js
@@ -103,19 +103,19 @@
 
 
 
-  // Setup
-function abTest(a, b) {
-  // Only change code below this line
-if (a<0 || b<0)
-return undefined
+//   // Setup
+// function abTest(a, b) {
+//   // Only change code below this line
+// if (a<0 || b<0)
+// return undefined
 
 
-  // Only change code above this line
+//   // Only change code above this line
 
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
-}
+//   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+// }
 
-abTest(2,2);
+// abTest(2,2);
 
 
 
@@ -180,5 +180,6 @@ const testObj = {
 };
 
 // Only change code below this line
+
 const entreeValue = testObj["an entree"];   // Change this line
 const drinkValue = testObj["the drink"];    // Change this line
