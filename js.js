357beb01de73adf4273db72e6320acfd37911c94
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-23 20:30:32 +0300

    Update js.js

--- js.js
+++ js.js
@@ -152,3 +152,33 @@ friends: ["dima","sasha"] array это массив только на русск
 
   // Only change code above this line
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
