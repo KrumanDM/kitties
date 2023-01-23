fc075d83b652f1b3f2715d099038ee7b1d569d24
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-23 20:45:24 +0300

    Update js.js

--- js.js
+++ js.js
@@ -122,20 +122,20 @@
 
 
 
-let count = 0;
+// let count = 0;
 
-function cc(card) {
-  // Only change code below this line
-  if (card <= 6) 
-    count++;
-  else if (card >="10")
-    count--;
-  return count + (count > 0 ? ' Bet' : ' Hold');
-}
-  // Only change code above this line
+// function cc(card) {
+//   // Only change code below this line
+//   if (card <= 6) 
+//     count++;
+//   else if (card >="10")
+//     count--;
+//   return count + (count > 0 ? ' Bet' : ' Hold');
+// }
+//   // Only change code above this line
 
 
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+// cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 
 
