dc7355f37232a75aa78cbcb82e056f4d81397f11
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-24 15:30:18 +0300

    Update js.js

--- js.js
+++ js.js
@@ -195,16 +195,16 @@
 
 
 
-// Setup
-const testObj = {
-  12: "Namath",
-  16: "Montana",
-  19: "Unitas"
-};
+// // Setup
+// const testObj = {
+//   12: "Namath",
+//   16: "Montana",
+//   19: "Unitas"
+// };
 
-// Only change code below this line
-const playerNumber = 16;  // Change this line
-const player = testObj[playerNumber];   // Change this line
+// // Only change code below this line
+// const playerNumber = 16;  // Change this line
+// const player = testObj[playerNumber];   // Change this line
 
 
 
