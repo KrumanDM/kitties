b45039d5ec5d80631197fe1b75509f4abf339488
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-24 15:30:34 +0300

    Update js.js

--- js.js
+++ js.js
@@ -218,16 +218,16 @@
 
 
 
-// Setup
-const myDog = {
-  "name": "Coder",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["freeCodeCamp Campers"]
-};
+// // Setup
+// const myDog = {
+//   "name": "Coder",
+//   "legs": 4,
+//   "tails": 1,
+//   "friends": ["freeCodeCamp Campers"]
+// };
 
-// Only change code below this line
-myDog.name = "Happy Coder"
+// // Only change code below this line
+// myDog.name = "Happy Coder"
 
 
 
