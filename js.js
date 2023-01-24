706dc6a9ad3aa09b142ffde6a8dec598c011237a
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-24 15:29:47 +0300

    Update js.js

--- js.js
+++ js.js
@@ -171,12 +171,12 @@
 
 
 
-// Setup
-const testObj = {
-  "an entree": "hamburger",
-  "my side": "veggies",
-  "the drink": "water"
-};
+// // Setup
+// const testObj = {
+//   "an entree": "hamburger",
+//   "my side": "veggies",
+//   "the drink": "water"
+// };
 
 // Only change code below this line
 
