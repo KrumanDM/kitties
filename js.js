a7123424be7b00dfc3f3e2a8788b5d002cf84b9f
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-01-30 20:16:06 +0300

    Update js.js

--- js.js
+++ js.js
@@ -103,39 +103,39 @@ function caseInSwitch(val) {
 
 
 
-//   // Setup
-// function abTest(a, b) {
-//   // Only change code below this line
-// if (a<0 || b<0)
-// return undefined
+  // Setup
+function abTest(a, b) {
+  // Only change code below this line
+if (a<0 || b<0)
+return undefined
 
 
-//   // Only change code above this line
+  // Only change code above this line
 
-//   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
-// }
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
 
-// abTest(2,2);
-
-
-
+abTest(2,2);
 
 
 
-// let count = 0;
-
-// function cc(card) {
-//   // Only change code below this line
-//   if (card <= 6) 
-//     count++;
-//   else if (card >="10")
-//     count--;
-//   return count + (count > 0 ? ' Bet' : ' Hold');
-// }
-//   // Only change code above this line
 
 
-// cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if (card <= 6) 
+    count++;
+  else if (card >="10")
+    count--;
+  return count + (count > 0 ? ' Bet' : ' Hold');
+}
+  // Only change code above this line
+
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 
 
