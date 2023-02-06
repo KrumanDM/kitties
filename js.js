fc649238f775e8af670dde3b65f50cf1990fb972
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-02-06 22:32:20 +0300

    Update js.js

--- js.js
+++ js.js
@@ -142,40 +142,40 @@ cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 
 
-// Setup
-const testObj = {
-  "an entree": "hamburger",
-  "my side": "veggies",
-  "the drink": "water"
-};
-
-// Only change code below this line
-
-const entreeValue = testObj["an entree"];   // Change this line
-const drinkValue = testObj["the drink"];    // Change this line
-
-
-
-
-
-
-
-
-
-
-
-
-
 // // Setup
 // const testObj = {
-//   12: "Namath",
-//   16: "Montana",
-//   19: "Unitas"
+//   "an entree": "hamburger",
+//   "my side": "veggies",
+//   "the drink": "water"
 // };
 
 // // Only change code below this line
-// const playerNumber = 16;  // Change this line
-// const player = testObj[playerNumber];   // Change this line
+
+// const entreeValue = testObj["an entree"];   // Change this line
+// const drinkValue = testObj["the drink"];    // Change this line
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
 
 
 
