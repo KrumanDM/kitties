7c066783dad010c367b90978aa66e9c8636829d8
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-02-06 22:31:45 +0300

    Update js.js

--- js.js
+++ js.js
@@ -142,17 +142,17 @@ cc(2); cc(3); cc(7); cc('K'); cc('A');
 
 
 
-// // Setup
-// const testObj = {
-//   "an entree": "hamburger",
-//   "my side": "veggies",
-//   "the drink": "water"
-// };
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
 
-// // Only change code below this line
+// Only change code below this line
 
-// const entreeValue = testObj["an entree"];   // Change this line
-// const drinkValue = testObj["the drink"];    // Change this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
 
 
 
