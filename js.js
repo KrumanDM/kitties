4aba7c5aba020c288af8757417dc107ef00c50a4
KrumanDM <120024603+KrumanDM@users.noreply.github.com>
2023-02-10 15:53:48 +0300

    Update js.js

--- js.js
+++ js.js
@@ -540,9 +540,9 @@ factorial(3);
 
 const dylan = [1,2,3,4,5,6,7,8,9]
 
-// for (let i=0; i < dylan.length; i++){
-//   console.log(dylan[i])
-// }
+for (let i=0; i < dylan.length; i++){
+  console.log(dylan[i])
+}
 
 // const newPeople = dylan.map(person =>{
 //   return person
